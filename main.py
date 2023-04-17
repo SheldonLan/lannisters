b71c72ef769da2f0e7f1c65e71f2e72dcbcfa015
SheldonRTA <sheldonlannister1@gmail.com>
2023-04-17 21:09:04 +0700

    invite + option choices

--- main.py
+++ main.py
@@ -8,12 +8,23 @@ import datetime
 
 import secret
 
+# consts
 guild_lannisters = 1097370199897939970
 servertime = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
-bot = commands.Bot()
+
+bot = commands.Bot(intents=nextcord.Intents.all())
+
+# Добро пожаловать
+@bot.event
+async def on_member_join(member: Member):
+    welcomeChannel = bot.get_channel(1097370868897816626)
+    embed = Embed(description=f"**Добро пожаловать {member.mention}!**")
+    embed.set_footer(text="Lannister's FAMQ")
+
+    await welcomeChannel.send(embed=embed)
 
 
-# правила
+# Правила
 @bot.slash_command(guild_ids=[guild_lannisters], description="[1-level] Правила")
 @application_checks.has_any_role('1-level')
 async def rules(interaction: Interaction):
@@ -33,12 +44,18 @@ async def rules(interaction: Interaction):
 @application_checks.has_any_role('Контракты')
 async def контракт(
         interaction: Interaction,
-        contract_name: str = SlashOption(description="Какой контракт Вы взяли?", required=True)):
+        contract_name: str = SlashOption(description="Какой контракт Вы взяли?",
+                                         required=True,
+                                         choices={"Большой улов": "Большой улов (рыбка)",
+                                                  "Грандиозная уборка": "Грандиозная уборка (мусор)",
+                                                  "Мясной день": "Мясной день (мясо)",
+                                                  "Долгожданная встреча": "Долгожданная встреча (схемы)",
+                                                  "Ломать - не строить": "Ломать - не строить",
+                                                  })):
 
     contractStartEmbed = Embed(
-        title=f"Может брать контракт {contract_name}!",
-        description=f"<@{interaction.user.id}> взял контракт {contract_name}.\n"
-                    f"Время на выполнение: ",
+        title=f"Внимание!",
+        description=f"<@{interaction.user.id}> взял контракт {contract_name}.",
         colour=nextcord.Colour.dark_gold())
 
     contractStartEmbed.set_footer(text=f"Время взятия: {servertime.strftime('%d.%m.%Y %H:%M:%S')}\n"
@@ -51,13 +68,9 @@ async def контракт(
         description=f"{contract_name} взятый ранее {interaction.user.name} откатился!",
         colour=nextcord.Colour.dark_blue()
     )
-    couldown = ((servertime + datetime.timedelta(hours=26)) - servertime).total_seconds()
+    couldown = ((servertime + datetime.timedelta(seconds=26)) - servertime).total_seconds()
     await asyncio.sleep(couldown)
-    await interaction.send(embed=contractTimeoutEmbed, ephemeral=False)
-
-
-
-
+    await interaction.send(content="<@&1097373637381726368>", embed=contractTimeoutEmbed, ephemeral=False)
 
 
 
