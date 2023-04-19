fa4423f6359c47e368324e3baa8a889eb3c7f58a
SheldonRTA <sheldonlannister1@gmail.com>
2023-04-19 15:33:41 +0700

    welcomeMessage and auto roleGiving+ Contracts and button w cd + Rules + purge + jokes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from nextcord import *
 from nextcord.ext import commands, application_checks
 from nextcord.ui import Button, View
 from translate import Translator
+import texttoimage
 
 import secret
 
@@ -19,7 +20,6 @@ servertime = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
 
 names = ["Яриком", "Милой", "Толей", "Валдисом", "Сергеем"]
 
-
 random_name = random.choice(names)
 
 # Activity
@@ -27,17 +27,19 @@ activity = nextcord.Activity(type=nextcord.ActivityType.watching, name=f"за {r
 
 bot = commands.Bot(intents=nextcord.Intents.all(), activity=activity, command_prefix="!")
 
+
 # Добро пожаловать
 @bot.event
 async def on_member_join(member: Member):
     welcomeChannel = bot.get_channel(1097370868897816626)
-    embed = Embed(title="Добро пожаловать!", description=f"**Рады приветствовать тебя, {member.mention}!\nНадеюсь, тебе понравится здесь.**")
-    embed.set_image("https://cdn-longterm.mee6.xyz/plugins/commands/images/762281010648842261/08734025084d41c21139210c8256695bfe62f6647349672c3bc194814f0cecda.gif")
+    embed = Embed(title="Добро пожаловать!",
+                  description=f"**Рады приветствовать тебя, {member.mention}!\nНадеюсь, тебе понравится здесь.**")
+    embed.set_image(
+        "https://cdn-longterm.mee6.xyz/plugins/commands/images/762281010648842261/08734025084d41c21139210c8256695bfe62f6647349672c3bc194814f0cecda.gif")
     embed.set_footer(text="Lannister's FAMQ")
     role = nextcord.utils.get(member.guild.roles, name="4-level")
     await member.add_roles(role)
     await welcomeChannel.send(embed=embed)
-
 
 
 # Правила
@@ -46,14 +48,14 @@ async def on_member_join(member: Member):
 async def правила(interaction: Interaction):
     channelRules = bot.get_channel(1097373322947342366)
     await channelRules.send("**Привет всем!** 😱\nЯ Ваш персональный помощник по всем вопросам.\n\n"
-                           "Первое с чем **нужно** ознакомиться - это правила. Уверяю, их немного.\n"
-                           "```1. Всё что происходит в этом дискорде - остаётся в этом дискорде\n"
-                           "2. Взаимовежливость - основа адекватного коммьюнити.\n"
-                           "3. В почти каждом канале есть закреп, пожалуйста, ознакомьтесь, прежде чем что-то писать.\n"
-                           "4. Проводите время совместно и приятно :)```\n"
-                           "Касаемо бота обращаться к <@265087722853498880> / <@504694073525796884>\n"
-                           "Перед любой slash-командой есть значок \"[role]\", который показывает уровень доступа к команде."
-                           "Без этой роли. у Вас не получится отправить команду (она не будет обработана).")
+                            "Первое с чем **нужно** ознакомиться - это правила. Уверяю, их немного.\n"
+                            "```1. Всё что происходит в этом дискорде - остаётся в этом дискорде\n"
+                            "2. Взаимовежливость - основа адекватного коммьюнити.\n"
+                            "3. В почти каждом канале есть закреп, пожалуйста, ознакомьтесь, прежде чем что-то писать.\n"
+                            "4. Проводите время совместно и приятно :)```\n"
+                            "Касаемо бота обращаться к <@265087722853498880> / <@504694073525796884>\n"
+                            "Перед любой slash-командой есть значок \"[role]\", который показывает уровень доступа к команде."
+                            "Без этой роли. у Вас не получится отправить команду (она не будет обработана).")
 
 
 choices = {"Большой улов": "Большой улов (рыбка)",
@@ -63,6 +65,7 @@ choices = {"Большой улов": "Большой улов (рыбка)",
            "Ломать - не строить": "Ломать - не строить",
            }
 
+
 # Контракт
 @bot.slash_command(guild_ids=[guild_lannisters], description="[Контракты] Взятие контракта")
 @application_checks.has_any_role('Контракты')
@@ -71,7 +74,6 @@ async def контракт(
         contract_name: str = SlashOption(description="Какой контракт Вы взяли?",
                                          required=True,
                                          choices=choices)):
-
     # Объявление и добавление кнопки
     accept_button = Button(label="Контракт выполнен!", style=ButtonStyle.green, custom_id="accept_button")
     view = View()
@@ -83,11 +85,11 @@ async def контракт(
         colour=nextcord.Colour.dark_gold())
     contractStartEmbed.set_footer(text=f"Время взятия: {servertime.strftime('%d.%m.%Y %H:%M:%S')}")
 
-    contractEndedEmbed = Embed(title=f"Контракт {contract_name} выполнен!", description=f"Контракт {contract_name} выполнен.\nПодтвердил действие {interaction.user.name}.")
+    contractEndedEmbed = Embed(title=f"Контракт {contract_name} выполнен!",
+                               description=f"Контракт {contract_name} выполнен.\nПодтвердил действие {interaction.user.name}.")
     contractEndedEmbed.set_footer(text=f"Время выполнения: " + servertime.strftime('%d.%m.%Y %H:%M:%S'))
 
     await interaction.send(embed=contractStartEmbed, ephemeral=False, view=view)
-
 
     async def on_button_click(interaction: Interaction):
         # Удаляем кнопку из представления
@@ -104,7 +106,8 @@ async def контракт(
 
             couldown = ((servertime + datetime.timedelta(seconds=5)) - servertime).total_seconds()
             await asyncio.sleep(couldown)
-            await bot.get_channel(interaction.channel_id).send(content="<@&1097373637381726368>", embed=contractTimeoutEmbed)
+            await bot.get_channel(interaction.channel_id).send(content="<@&1097373637381726368>",
+                                                               embed=contractTimeoutEmbed)
 
         elif contract_name == "Грандиозная уборка (мусор)":
             contractTimeoutEmbed = Embed(
@@ -115,7 +118,8 @@ async def контракт(
 
             couldown = ((servertime + datetime.timedelta(seconds=5)) - servertime).total_seconds()
             await asyncio.sleep(couldown)
-            await bot.get_channel(interaction.channel_id).send(content="<@&1097373637381726368>", embed=contractTimeoutEmbed)
+            await bot.get_channel(interaction.channel_id).send(content="<@&1097373637381726368>",
+                                                               embed=contractTimeoutEmbed)
 
         elif contract_name == "Мясной день (мясо)":
             contractTimeoutEmbed = Embed(
@@ -126,7 +130,8 @@ async def контракт(
 
             couldown = ((servertime + datetime.timedelta(seconds=5)) - servertime).total_seconds()
             await asyncio.sleep(couldown)
-            await bot.get_channel(interaction.channel_id).send(content="<@&1097373637381726368>", embed=contractTimeoutEmbed)
+            await bot.get_channel(interaction.channel_id).send(content="<@&1097373637381726368>",
+                                                               embed=contractTimeoutEmbed)
         elif contract_name == "Долгожданная встреча (схемы)":
             contractTimeoutEmbed = Embed(
                 title=f"Контракт откатился!",
@@ -136,7 +141,8 @@ async def контракт(
 
             couldown = ((servertime + datetime.timedelta(seconds=5)) - servertime).total_seconds()
             await asyncio.sleep(couldown)
-            await bot.get_channel(interaction.channel_id).send(content="<@&1097373637381726368>", embed=contractTimeoutEmbed)
+            await bot.get_channel(interaction.channel_id).send(content="<@&1097373637381726368>",
+                                                               embed=contractTimeoutEmbed)
         else:
             contractTimeoutEmbed = Embed(
                 title=f"Контракт откатился!",
@@ -149,10 +155,9 @@ async def контракт(
             await bot.get_channel(interaction.channel_id).send(content="<@&1097373637381726368>",
                                                                embed=contractTimeoutEmbed)
 
-
-
     # Привязываем обработчик событий к кнопке
     accept_button.callback = on_button_click
+
 
 # шутка
 @bot.slash_command(guild_ids=[guild_lannisters], description="[2-level] Получение рандомной шутки")
