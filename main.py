eeddadd77e3a96c5b916d744fefbbb2bfba4aeb3
SheldonRTA <sheldonlannister1@gmail.com>
2023-06-13 15:43:14 +0700

    Events: auto-role, activity-changer
    Commands:
    /взп
    /контракт
    /шутка
    /очистить
    !restart

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ guild_lannisters = 1097370199897939970  # id дискорд сервера
 servertime = datetime.datetime.utcnow() + datetime.timedelta(hours=3)  # получение мск времени
 
 # Activity
-names = cycle(["за Яриком", "за Милой", "за Толей", "за Валдисом", "за Сергеем", "за Данилом", "за Иваном", "за Настей", "за Алисой", "за Олегом", "за Наташей", "за Антоном", "за Львом"])
+names = cycle(["за Яриком", "за Милой", "за Толей", "за Валдисом", "за Сергеем", "за Данилом", "за Иваном", "за Настей", "за Алисой", "за Олегом", "за Наташей", "за Антоном", "за Львом", "за Денисом"])
 
 
 # Bot definition
@@ -50,7 +50,7 @@ async def on_member_join(member: Member):
     embed.set_image(
         "https://cdn-longterm.mee6.xyz/plugins/commands/images/762281010648842261/08734025084d41c21139210c8256695bfe62f6647349672c3bc194814f0cecda.gif")
     embed.set_footer(text="Lannister's FAMQ")
-    role = nextcord.utils.get(member.guild.roles, name="4-level")
+    role = nextcord.utils.get(member.guild.roles, name="Гость")
     await member.add_roles(role)
     await welcomeChannel.send(embed=embed)
 
@@ -209,252 +209,11 @@ async def очистить(interaction: Interaction,
     await bot.get_channel(interaction.channel_id).purge(limit=1)
 
 
-"""
-Обычные команды для работы с музыкой:
-!play url - для воспроизведения одного трека
-!stop - для остановки воспроизведения (можно использовать как /skip)
-!pause - для паузы
-!resume - для продолжения
-!leave - покинуть голосовой канал
-"""
-
-
-@bot.command()
-async def play(ctx, url: str):
-    re = requests.get(url).text
-    soup = BeautifulSoup(re, "html.parser")
-    filename = soup.find('title').text.replace(" - YouTube", "")
-    if os.path.isfile(f'D:/tmp/{filename}.mp4'):
-        await ctx.send("У меня есть эта песня!")
-        voice_channel = ctx.author.voice.channel
-        voice_client = await voice_channel.connect()
-        source = await nextcord.FFmpegOpusAudio.from_probe(f'D:/tmp/{filename}.mp4')
-        voice_client.play(source)
-        voicePlayEmbed = Embed(description=f"Cейчас играет: {filename}", colour=nextcord.Colour.random())
-        voicePlayEmbed.set_footer(text=f"Приятного прослушивания, {ctx.author}")
-        await ctx.send(embed=voicePlayEmbed)
-        while voice_client.is_playing():
-            await asyncio.sleep(2)
-        await voice_client.disconnect()
-    else:
-        try:
-            await ctx.send(
-                "<a:musica53:1098869634880503908> Подождите пару секунд, я загружаю песню. <a:musica53:1098869634880503908>")
-            yt = YouTube(url)
-            await asyncio.sleep(3)
-            stream = yt.streams.filter(only_audio=True).first()
-            await asyncio.sleep(2)
-            filename = stream.download(output_path='D:/tmp')
-            voice_channel = ctx.author.voice.channel
-            voice_client = await voice_channel.connect()
-            source = await nextcord.FFmpegOpusAudio.from_probe(f'{filename}')
-            voice_client.play(source)
-            voicePlayEmbed = Embed(description=f"Cейчас играет: {filename.replace('D:/tmp', '').replace('.mp4', '')}",
-                                   colour=nextcord.Colour.random())
-            voicePlayEmbed.set_footer(text=f"Приятного прослушивания, {ctx.author}")
-            await ctx.send(embed=voicePlayEmbed)
-            while voice_client.is_playing():
-                await asyncio.sleep(2)
-            await voice_client.disconnect()
-        except KeyError:
-            try:
-                await ctx.send(
-                    "<a:srt_discordloading:1098494332991963157> Не получилось загрузить песню, попробую по другому. <a:srt_discordloading:1098494332991963157>")
-                yt = YouTube('https://youtu.be/' + url.split('watch?v=')[1].split('&')[0])
-                stream = yt.streams.filter(only_audio=True).first()
-                filename = stream.download(output_path='D:/tmp')
-                voice_channel = ctx.author.voice.channel
-                voice_client = await voice_channel.connect()
-                source = await nextcord.FFmpegOpusAudio.from_probe(f'{filename}')
-                voice_client.play(source)
-                voicePlayEmbed = Embed(
-                    description=f"Cейчас играет: {filename.replace('D:/tmp', '').replace('.mp4', '')}",
-                    colour=nextcord.Colour.random())
-                voicePlayEmbed.set_footer(text=f"Приятного прослушивания, {ctx.author}")
-                await ctx.send(embed=voicePlayEmbed)
-                while voice_client.is_playing():
-                    await asyncio.sleep(2)
-                await voice_client.disconnect()
-            except KeyError:
-                await ctx.send(
-                    "<a:ghost64:1098869622645735565> Вообще не могу получить доступ :c\nВоспользуйтесь командой !restart, а после повторите попытку. <a:ghost64:1098869622645735565>")
-
-
-@bot.command()
-async def pause(ctx):
-    vc = ctx.voice_client
-    try:
-        if vc.is_playing():
-            vc.pause()
-    except AttributeError:
-        await ctx.send("Я ничего сейчас не играю.")
-
-
-@bot.command()
-async def resume(ctx):
-    vc = ctx.voice_client
-    try:
-        if vc.is_paused():
-            vc.resume()
-    except AttributeError:
-        await ctx.send("Я не на паузе.")
-
-
-@bot.command()
-async def stop(ctx):
-    vc = ctx.voice_client
-    try:
-        if vc.is_playing():
-            vc.stop()
-    except AttributeError:
-        await ctx.send("Я ничего не играю")
-
-
-@bot.command()
-async def leave(ctx):
-    voice_client = ctx.guild.voice_client
-    try:
-        if voice_client.is_connected():
-            await voice_client.disconnect()
-    except AttributeError:
-        await ctx.send("Я не нахожусь в голосовом канале!")
-
-
 @bot.command()
 async def restart(ctx):
     await ctx.send('Перезапускаюсь...')
     subprocess.run(['bat.bat'])
     os.execl(sys.executable, sys.executable, "C:/Users/RTA-Telecom/Desktop/test files/lannisters/main.py")
-
-
-"""to-do"""
-
-
-@bot.slash_command(name="to-do")
-async def todo(interaction: Interaction):
-    await interaction.send("1. Дни рождения участников set/remove/remember\n"
-                           "2. ")
-
-
-"""
-
-Новый самописный музыкальный плеер
-Команды:
-/play voice_channel- Воспроизведение очереди треков в голосовом канале
-/add url- добавление  трека в очередь, по url идёт проверка, существует ли файл, если нет, то скачивается и добавляет в очередь
-/showqueue - вывод Embed сообщения с очередью треков
-/clearqueue - очистка очереди
-/search - поиск по тексту
-"""
-@bot.slash_command(guild_ids=[guild_lannisters], description="Поиск видео по тексту")
-async def search(interaction: Interaction, text: str = SlashOption(description="Введите текст для поиска", required=True)):
-
-    search_text = text
-    channel = bot.get_channel(interaction.channel_id)
-    save_dir = 'D:/tmp/'
-
-    html_page = requests.get(f"https://ru.hitmotop.com/search?q={search_text}").content
-    soup = BeautifulSoup(html_page, 'html.parser')
-
-    track_info = soup.find_all("li", {"class": "tracks__item"})[:5]
-    track_list = []
-
-    for track in track_info:
-        track_parser = track.text.strip()
-        title = track_parser.split("\n")[0]
-        author = track_parser.split("\n")[2]
-        duration = track_parser.split("\n")[-1]
-        download_track = track.find("a", {"class": "track__download-btn"}).get('href')
-        track_list.append(author + "-" + title + " (" + duration + ") " + download_track)
-    await interaction.send(f"Поиск по {search_text} выполнен")
-
-    track_option = []
-    for element in track_list:
-        element_author = element.split("-")[0]
-        element_title = element.split("-")[1].split(" (")[0]
-        element_duration = element.split(" (")[1].split(")")[0]
-        element_label = element_author + " - " + element_title + " (" + element_duration + ")"
-        element_value = element.split(")")[1].strip()
-        select_option = SelectOption(label=element_label, value=element_value, description=element_label)
-        track_option.append(select_option)
-
-    select_menu = nextcord.ui.Select(placeholder="Выберите песню", options=track_option)
-
-    async def handle_select(interaction: nextcord.Interaction, select_menu: nextcord.ui.Select):
-        selected_option = select_menu.values[0]
-        filename = f"{selected_option.split('/')[-1]}"
-        urllib.request.urlretrieve(selected_option, os.path.join(save_dir, filename))
-        with open("queue.txt", 'a', encoding="utf-8") as queue:
-            queue.write(f"{filename}\n")
-
-
-    select_menu.callback = partial(handle_select, select_menu=select_menu)
-
-    view = View()
-    view.add_item(select_menu)
-    await interaction.edit_original_message(view=view)
-
-@bot.slash_command(guild_ids=[guild_lannisters], description="Добавление песни по URL")
-async def add(interaction: Interaction,
-              url: str = SlashOption(description="Ссылка на youtube", required=True)):
-    # Отправка запроса по URL для получения названия файла
-    re = requests.get(url).text
-    soup = BeautifulSoup(re, "html.parser")
-    filename = soup.find('title').text.replace(" - YouTube", "")
-    # Проверка на существование файла
-    if os.path.isfile(f'D:/tmp/{filename}.mp4'):
-        with open("queue.txt", 'a', encoding="utf-8") as queue:
-            queue.write(f"{filename.replace('D:/tmp', '')}\n")
-            addEmbed = Embed(description=f"Файл {filename} был добавлен в очередь воспроизведения!",
-                             colour=nextcord.Colour.red())
-        await interaction.send(embed=addEmbed, ephemeral=True)
-    else:
-        await interaction.send("Скачиваю файл.", ephemeral=True)
-        yt = YouTube(url)
-        stream = yt.streams.filter(only_audio=True).first()
-        filename = stream.download(output_path='D:/tmp')
-        with open("queue.txt", 'a', encoding="utf-8") as queue:
-            queue.write(f"{filename.replace('D:/tmp', '')[1:]}\n")
-            downloadAddEmbed = Embed(
-                description=f"Файл {filename.replace('D:/tmp', '')[1:].replace('.mp4', '')} был добавлен в очередь воспроизведения!",
-                colour=nextcord.Colour.red())
-        await interaction.edit_original_message(content="", embed=downloadAddEmbed)
-
-@bot.slash_command(guild_ids=[guild_lannisters], description="Удаление списка песен")
-async def clearqueue(interaction: Interaction):
-    with open("queue.txt", 'w', encoding='utf-8') as file:
-        file.write('')
-    clearEmbed = Embed(description=f"Очередь была очищена {interaction.user.name}", colour=nextcord.Colour.random())
-    await interaction.send(embed=clearEmbed)
-
-
-@bot.slash_command(guild_ids=[guild_lannisters], description="show queue")
-async def showqueue(interaction: Interaction):
-    with open('queue.txt', 'r', encoding="utf-8") as queue:
-        lines = queue.read().split('\n')
-        message = "\n".join(lines)
-        queueEmbed = Embed(title="Очередь воспроизведения:", description=f"{message}", colour=nextcord.Colour.random())
-        await interaction.send(embed=queueEmbed)
-
-
-@bot.slash_command(guild_ids=[guild_lannisters], description="play test")
-async def play(interaction: Interaction, channel: VoiceChannel):
-    voice_client = await channel.connect()
-    with open('queue.txt', 'r', encoding="utf-8") as queue:
-        queueList = queue.read().strip().split('\n')
-    if len(queueList) == 0:
-        await interaction.send("Очередь пуста")
-    else:
-        await interaction.send("Запускаюсь")
-        for element in queueList:
-            source = await nextcord.FFmpegOpusAudio.from_probe(f'D:/tmp/{element}')
-            voice_client.play(source)
-            voicePlayEmbed = Embed(description=f"Cейчас играет: {element.replace('mp3', '')}", colour=nextcord.Colour.random())
-            voicePlayEmbed.set_footer(text=f"Приятного прослушивания, {interaction.user.name}")
-            await interaction.edit_original_message(content="", embed=voicePlayEmbed)
-            while voice_client.is_playing():
-                await asyncio.sleep(5)
-        await voice_client.disconnect()
 
 
 """
